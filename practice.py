9fdedddf2bb491e0a7afe4416095420d4b583cef
JustinLi3 <lijustin83@gmail.com>
2024-10-04 12:32:39 -0400

    saving changes

--- practice.py
+++ practice.py
@@ -1,16 +1,12 @@
-import os
 from pyspark.sql import SparkSession
-
-# Set the HADOOP_USER_NAME to bypass Hadoop's user authentication
-os.environ['HADOOP_USER_NAME'] = 'hadoop'
 
 # Start a Spark session
 spark = SparkSession.builder.appName('Practice').getOrCreate()
-
-# Set log level to ERROR to reduce verbosity
-spark.sparkContext.setLogLevel("ERROR")
 
 # Read dataset with Spark (with header and inferred schema)
 df_pyspark = spark.read.csv('practice.csv', header=True, inferSchema=True)
 
 df_pyspark.show()
+
+spark.stop()
+
