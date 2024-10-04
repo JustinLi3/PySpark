9e7c65ff274356209f8aeb1a648605bf072cf97e
JustinLi3 <lijustin83@gmail.com>
2024-10-04 00:36:37 -0400

    saving

--- practice.py
+++ practice.py
@@ -1,20 +1,7 @@
-import os
-os.environ['PYSPARK_PYTHON'] = '/path/to/python'
 from pyspark.sql import SparkSession
-
 # Initialize a Spark session
 spark = SparkSession.builder \
     .appName("PySpark Test") \
     .getOrCreate()
-
-# Create a simple DataFrame
-data = [("John", 28), ("Jane", 33), ("Sam", 41)]
-columns = ["Name", "Age"]
-
-df = spark.createDataFrame(data, columns)
-
-# Show the DataFrame
-df.show()
-
 # Stop the Spark session
 spark.stop()
