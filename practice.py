bc4c00fccf4ca7d4362b458bf46f23259fe47d79
JustinLi3 <lijustin83@gmail.com>
2024-10-04 12:13:30 -0400

    saving

--- practice.py
+++ practice.py
@@ -1,7 +1,21 @@
+# Import the required module
 from pyspark.sql import SparkSession
+
 # Initialize a Spark session
 spark = SparkSession.builder \
-    .appName("PySpark Test") \
+    .appName("Simple PySpark Test") \
+    .master("local[*]") \
     .getOrCreate()
+
+# Create a simple DataFrame
+data = [("John", 28), ("Anna", 23), ("Mike", 35)]
+columns = ["Name", "Age"]
+
+# Create DataFrame
+df = spark.createDataFrame(data, columns)
+
+# Show the DataFrame
+df.show()
+
 # Stop the Spark session
 spark.stop()
